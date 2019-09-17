d080fb150d5ff78d7741d196bd43c0a6a8edc00b
Evan Sampson <evan.sampson@fieldstack.com>
2019-09-17 16:30:59 -0400

    guarentee space in layout before the image loads in Add a slight delay before the screen stops being blue, less jarring

--- javascript/home.js
+++ javascript/home.js
@@ -2,12 +2,12 @@ function revealHeader()
 {
     var header = document.getElementsByTagName("header")[0];
     var loading = header.getElementsByClassName("loading")[0];
-    loading.style.animation = "fadeOutHeader ease-in 2s 1";
+    loading.style.animation = "fadeOutHeader ease-in 3s 1";
     loading.style.opacity = 0;
     var headerImage = header.getElementsByTagName("img")[0];
-    headerImage.style.animation = "fadeInHeader ease-in 2s 1";
+    headerImage.style.animation = "fadeInHeader ease-in 3s 1";
     headerImage.style.opacity = 0.75;
     var titles = header.getElementsByClassName("titles")[0];
-    titles.style.animation = "fadeInTitle ease-in 3s 1";
+    titles.style.animation = "fadeInTitle ease-in 4s 1";
     titles.style.opacity = 1;
 }
